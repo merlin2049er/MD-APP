d134eb7149ef6703ad705a8d2d81f45021ea22dc
Joe Guerra <guerra_joe@hotmail.com>
2016-10-30 18:00:11 +0000

    trying to correct email and sendgrid

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,7 @@
 class ContactsController < ApplicationController
  
 def index
-   redirect_to '/contact' 
+# redirect_to '/contact' 
 end
 
  def new
@@ -12,17 +12,17 @@ end
  @contact = Contact.new(contact_params)
 
  if @contact.save
- redirect_to root_path
- name = params[:contact][:name]
- email = params[:contact][:email]
- message = params[:contact][:message]
- ContactMailer.contact_email(name, email, message).deliver
- flash[:success] = "Thanks for the message, we will be in touch soon."
+    redirect_to root_path
+    name = params[:contact][:name]
+    email = params[:contact][:email]
+    message = params[:contact][:message]
+    ContactMailer.contact_email(name, email, message).deliver
+    flash[:success] = "Thanks for the message, we will be in touch soon."
  else
- redirect_to pages_contact_path
- flash[:danger] = "Opps, there was a problem! Please fill out all the fields."
-
+    redirect_to pages_contact_path
+    flash[:danger] = "Opps, there was a problem! Please fill out all the fields."
  end
+ 
  end
 
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  config.action_mailer.default_url_options = { host: 'https://massdump-merlin2049er.c9users.io/', port: 3000 }
 end
