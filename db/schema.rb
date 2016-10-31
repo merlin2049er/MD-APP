3c364ee1e1af166296396e48671cbd8394f94db6
Joe Guerra <guerra_joe@hotmail.com>
2016-10-31 17:24:46 +0000

    changed schema, still no good?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161030220031) do
+ActiveRecord::Schema.define(version: 20161031150937) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
