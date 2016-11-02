a6204a9e76ea864e888e648429764a550169055d
Joe Guerra <guerra_joe@hotmail.com>
2016-11-02 13:46:48 +0000

    cleaned up some html

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,8 @@ class RegistrationsController < Devise::RegistrationsController
  
   add_breadcrumb "MASSDUMP", :root_path
   #add_breadcrumb  "sign up", :new_user_registration
-
+  #add_breadcrumb "sign up", :contacts_path
+  
  
   def edit
    add_breadcrumb "profile"
