4ffa717787f69a9ac74e77c54d795693949c5ec9
Joe Guerra <guerra_joe@hotmail.com>
2016-11-12 13:54:41 +0000

    working on sort or of categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,18 +5,25 @@ class Category < ActiveRecord::Base
    validates_presence_of :name
 
 def edit
-    
 end
 
 def index
-
-    
 end
 
     
 def show
+end
     
+    
+def self.order_list(sort_order)
+    if sort_order == "newest" || sort_order.blank?
+       order(created_at :desc)
+    else if sort_order == "name"
+        order(created_at :name)
+    else
+         order(created_at :asc)
+    end
+    end
+end 
 end
 
-    
-end
