477e56635e44a9f086a1be1a5bf8662e2f0cf105
Joe Guerra <guerra_joe@hotmail.com>
2016-11-17 01:20:53 +0000

    added route to cart

---
+++ app/controllers/modal_controller.rb
@@ -0,0 +1,17 @@
+class ModalController < ApplicationController
+  
+  add_breadcrumb "MASSDUMP", :root_path
+#  add_breadcrumb "contacts", :contact_path
+#  add_breadcrumb "about", :about_path
+#  add_breadcrumb "error", :error_path
+  
+  def index
+    
+  end
+
+
+  def add_to_cart 
+     
+  end
+  
+end
