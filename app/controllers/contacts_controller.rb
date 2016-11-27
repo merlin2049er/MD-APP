5a5fe187693496e9a8c81e1923725e461adfbf8b
Joe Guerra <guerra_joe@hotmail.com>
2016-11-27 12:07:34 -0500

    added date to notifications, and transactions. added funded => false to query

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,6 +13,9 @@ class ContactsController < ApplicationController
     name = params[:contact][:name]
     email = params[:contact][:email]
     message = params[:contact][:message]
+    supplier = params[:contact][:message]
+    registered_user = params[:contact][:message]
+
     ContactMailer.contact_email(name, email, message).deliver
     flash[:success] = "Thanks for the message, we will be in touch soon."
  else
@@ -27,6 +30,6 @@ class ContactsController < ApplicationController
  private
 
  def contact_params
- params.require(:contact).permit(:name, :email, :message)
+ params.require(:contact).permit(:name, :email, :supplier, :registered_user, :message)
  end
 end
