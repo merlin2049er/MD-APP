8b2ad8d88c0279e7c9e578d383ac693cef5dede6
joeguerra <zorkmerlin>
2017-08-23 20:35:27 -0400

    added requests models / migration

---
+++ app/models/add_manufacturerurl_to_request.rb
@@ -0,0 +1,2 @@
+class AddManufacturerurlToRequest < ActiveRecord::Base
+end
---
+++ test/models/create_request_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CreateRequestTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
