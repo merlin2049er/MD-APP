94e86bd8d81f528896dfcac7b64dc1dbc8b08c09
Deetss <dylan817@live.com>
2017-12-11 16:07:13 +0000

    Move qty select to same line as checkout button

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111155558) do
+ActiveRecord::Schema.define(version: 20171211154002) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
