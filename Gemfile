c09885c2ca47f0f866876ace9a89b32d2c95448a
joeguerra <zorkmerlin>
2017-12-31 12:07:07 -0500

    little tiny notes in panel heading bars

--- Gemfile
+++ Gemfile
@@ -103,6 +103,7 @@ gem 'carrierwave'
 gem 'mini_magick'
 
 
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,5 +11,3 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( ckeditor/*)
-Rails.application.config.assets.precompile += %w( modal.css )
-Rails.application.config.assets.precompile += %w( modal_wherecheckout.js )
