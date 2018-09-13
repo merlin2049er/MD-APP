52eb36ac32583b69097e2807f0983444fd4c9aa7
Joe Guerra <merlin2049er@gmail.com>
2018-09-13 15:03:59 -0400

    fixed user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable,
-         :omniauthable, omniauth_providers: [:facebook]
+         :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :notifications
   has_many :transactions
