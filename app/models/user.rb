b335269e8d9f02e7078475c8ea6483ef1c449264
Joe Guerra <merlin2049er@gmail.com>
2018-09-13 15:19:45 -0400

    added something to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,13 @@ class User < ActiveRecord::Base
 
   acts_as_commontator
 
+  def self.new_with_session(params, session)
+      super.tap do |user|
+        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+          user.email = data["email"] if user.email.blank?
+        end
+      end
+    end
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
