1ba750b9eea1061b382e658d332b895498bb4ded
Joe Guerra <merlin2049er@gmail.com>
2018-10-01 16:31:06 -0400

    facebook api stuff 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,12 +24,13 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.username = auth.info.name   # assuming the user model has a name
+      user.firstname = auth.info.first_name   # assuming the user model has a name
+      user.lastname = auth.info.last_name   # assuming the user model has a name
       # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
        user.skip_confirmation!
-
+       user.save!
     end
   end
 
