e91dbaef50854734344d833f968ce50b68fb7de9
Joe Guerra <merlin2049er@gmail.com>
2018-10-01 10:16:04 -0400

    facebook integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ActiveRecord::Base
       # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+       user.skip_confirmation!
     end
   end
 
