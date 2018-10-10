9288ec97ec1335f784e293d1bc088d058d432714
Joe Guerra <merlin2049er@gmail.com>
2018-10-10 13:12:18 -0400

    remove captcha

--- config/initializers/recaptcha.rb
+++
@@ -1,5 +0,0 @@
-Recaptcha.configure do |config|
-  config.site_key   =  ENV['6Lc5YXQUAAAAAB1hSf1HJuJk-wuvUiiQgl9QH5nB']
-  config.secret_key  = ENV['6Lc5YXQUAAAAAHOAT2_aZaxa_5CE_OfqwlUY59HC']
-
-end
