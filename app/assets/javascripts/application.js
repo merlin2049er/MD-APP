1a2d2a6340134fd1d285751ec56275c9f7a90b27
Joe Guerra <merlin2049er@gmail.com>
2019-03-27 15:15:20 -0400

    ..

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require turbolinks
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require ckeditor/override
 //= require ckeditor/init
 //= require lightbox
 //= require rails.validations
