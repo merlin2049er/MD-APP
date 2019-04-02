66f9426cbeb5025dcc6d334acecbcf2a5e1a000c
Joe Guerra <merlin2049er@gmail.com>
2019-04-02 16:38:48 -0400

    add config.serve_static_assets = true

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+config.serve_static_assets = true
+
 require File.expand_path('boot', __dir__)
 
 require 'rails/all'
@@ -25,5 +27,5 @@ module Workspace
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
-  
+
 end
