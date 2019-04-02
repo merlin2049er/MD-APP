fd9ec496db31b305fd86c4a2324fd7f7c19e0e2b
Joe Guerra <merlin2049er@gmail.com>
2019-04-02 16:40:10 -0400

    ..

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-config.serve_static_assets = true
+
 
 require File.expand_path('boot', __dir__)
 
@@ -26,6 +26,8 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.serve_static_assets = true
   end
 
 end
