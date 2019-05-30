cf4c8edd6846b8f5539447383ba67b9a8ddc2ca8
Joe Guerra <merlin2049er@gmail.com>
2019-05-30 09:17:06 -0400

    fix contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,10 +3,14 @@
 class ContactMailer < ActionMailer::Base
   default to: 'jguerra@jginfosys.com'
 
-  def contact_email(name, email, message)
+  def contact_email(name, email, message, registered_user, supplier)
+
     @name = name
     @email = email
     @message = message
+    @registered_user = registered_user
+    @supplier = supplier
+
 
     mail(from: email, subject: 'Enquiry message from Massdump')
   end
