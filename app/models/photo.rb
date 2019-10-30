a8cf28b4be2f80dcaeaddcd11eccbd2f9d764e97
joeguerra <guerra_joe@hotmail.com>
2019-10-30 18:01:43 -0400

    fix product form for photos and photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,6 +3,14 @@
 class Photo < ActiveRecord::Base
   belongs_to :product
 
+  after_initialize :set_defaults
+
   validates_presence_of :uri
-  validates_presence_of :product_id
+  # validates_presence_of :product_id
+  #
+
+  def set_defaults
+    self.enabled ||= true
+  end
+
 end
