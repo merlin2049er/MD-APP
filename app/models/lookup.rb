c225e398f8a2bab861b1c9e5617b3e8a98e361b1
joeguerra <guerra_joe@hotmail.com>
2019-11-15 13:02:52 -0500

    fixed nil taxrate

--- app/models/lookup.rb
+++ app/models/lookup.rb
@@ -3,7 +3,13 @@ class Lookup
   def self.getrate(user)
      puts"=-=-=-=",user.inspect
      taxrate = Tax.find_by_prov_id(user.state)
+
+    if taxrate.nil?
+        return tax_rate = 0.0
+    else
      return taxrate.tax_rate
+   end
+   
   end
 
 end
