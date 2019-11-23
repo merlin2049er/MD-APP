cbd23548bf9e3defb56ff87fb6b229225c2814a6
joeguerra <guerra_joe@hotmail.com>
2019-11-23 12:20:40 -0500

    remove keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-    :publishable_key => "pk_test_wNeuri3HuwlL2HgmMLaCfLxb00noMQ7VJG",
-    :secret_key      => "sk_test_GTNsI2Uc5BBqqWiHas7hYJlt00ruRDdxlo"
+    :publishable_key => "",
+    :secret_key      => ""
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
