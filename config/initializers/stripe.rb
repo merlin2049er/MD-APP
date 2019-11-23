9f3876f4e70505e67d30a4488cfb6320ec79a034
joeguerra <guerra_joe@hotmail.com>
2019-11-23 12:18:27 -0500

    stripe...

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-    :publishable_key => "",
-    :secret_key      => ""
+    :publishable_key => "pk_test_wNeuri3HuwlL2HgmMLaCfLxb00noMQ7VJG",
+    :secret_key      => "sk_test_GTNsI2Uc5BBqqWiHas7hYJlt00ruRDdxlo"
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
