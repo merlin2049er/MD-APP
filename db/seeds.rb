abe95f58e726922e83939fbdb329ffff5e75bd0f
Joe Guerra <guerra_joe@hotmail.com>
2020-01-13 16:47:10 -0800

    need to fix seed file... for taxe rates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,27 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   cities = City.create( name: 'Chicago' }, { name: 'Copenhagen' )
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# seed taxes (2019 rates)
+
+tax_list = [
+[ 'AB',  0.05 ],
+[ 'BC' ,   0.07],
+[ 'MB',   0.08],
+[ 'NB',   0.15],
+[ 'NL',   0.15],
+[ 'NT',   0.05],
+[ 'NS',   0.15],
+[ 'NU',   0.05],
+[ 'ON' ,   0.13 ],
+[ 'PE',   0.15],
+[ 'QC',   0.09975],
+[ 'SK',   0.06],
+[ 'YT'	,   0.05]
+]
+
+tax_list.each do |prov, rate|
+  Taxes.create( prov: prov_id, rate: tax_rate )
+end
