4e2f7143c171c7d042ea3a4052b67e8f834dce2d
Joe Guerra <guerra_joe@hotmail.com>
2020-02-11 10:05:05 -0500

    Update stripe.rb
    Set ENV variables

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,14 @@
+# setup strip keys
+
+#Rails.configuration.stripe = {
+#    :publishable_key => "",
+#    :secret_key      => ""
+# }
+
 Rails.configuration.stripe = {
-    :publishable_key => "",
-    :secret_key      => ""
-}
+     publishable_key: ENV['PUBLISHED_KEY'],
+          secret_key: ENV['SECRET_KEY']     
+    
+  }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
