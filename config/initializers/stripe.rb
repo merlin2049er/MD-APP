6bb42df7f3f402dcf6f28baf05f6f09e9b0a0cb5
saqibgujjar <39758201+saqibgujjar@users.noreply.github.com>
2020-02-13 00:38:25 +0500

    add

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,14 +1,9 @@
-# setup strip keys
 
-#Rails.configuration.stripe = {
-#    :publishable_key => "",
-#    :secret_key      => ""
-# }
+require 'stripe'
 
 Rails.configuration.stripe = {
-     publishable_key: ENV['PUBLISHED_KEY'],
-          secret_key: ENV['SECRET_KEY']     
-    
-  }
+    :publishable_key => "pk_test_wNeuri3HuwlL2HgmMLaCfLxb00noMQ7VJG",
+    :secret_key      => "sk_test_GTNsI2Uc5BBqqWiHas7hYJlt00ruRDdxlo"
+}
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
